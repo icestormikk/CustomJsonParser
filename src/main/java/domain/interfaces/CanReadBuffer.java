620fc55029977f67fb39d9d4b5cc0990446454cf
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-31 23:08:32 +0300

    Написал инструмент для взаимодействия с буффером для чтения

---
+++ src/main/java/domain/interfaces/CanReadBuffer.java
@@ -0,0 +1,27 @@
+package domain.interfaces;
+
+import java.io.IOException;
+import java.io.PushbackReader;
+
+public interface CanReadBuffer {
+
+    /**
+     * Получить следующий символ в файле (за исключением специальных символов)
+     * @param reader Объект класса BufferedReader, с помощью которого происходит посимвольное чтение
+     * @return Следующий символ в файле, не являеющийся специальным символом
+     */
+    default int getc(PushbackReader reader) {
+        try {
+            int c;
+            while ((c = reader.read()) != -1) {
+                if (!Character.isWhitespace(c)) {
+                    return c;
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return -1;
+    }
+}
