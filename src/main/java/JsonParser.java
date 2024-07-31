a0a19d10597fb27eb84389c1ab62f11a061e3016
Pavel Zhigalov <jigalow.pavel2015@yandex.ru>
2024-07-31 23:11:54 +0300

    Немного тестового кода

--- src/main/java/JsonParser.java
+++ src/main/java/JsonParser.java
@@ -1,14 +1,16 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import domain.abstraction.JsonPrimitive;
+import domain.abstraction.JsonPrimitiveParser;
+import domain.classes.exceptions.JsonObjectParserException;
+import domain.classes.exceptions.JsonParserException;
+import domain.classes.exceptions.JsonStringParserException;
+import domain.interfaces.CanReadBuffer;
+
+import java.io.*;
 
 /**
  * Основный класс парсера json-объектов
  */
-public class JsonParser {
+public class JsonParser implements CanReadBuffer {
     private final String filename;
 
     public JsonParser(String filename) {
@@ -16,40 +18,25 @@ public class JsonParser {
     }
 
     /**
-     * Распарсить файл, путь к которому был передан в конструкторе данного объекта JsonParser
+     * Распарсить файл, путь к которому был передан в конструкторе данного объекта JsonPrimitiveParser
      */
-    public void parse() {
+    public JsonPrimitive<?> parse() {
+        JsonPrimitive<?> result = null;
+
         try (
             InputStream inputStream = new FileInputStream(filename);
             Reader reader = new InputStreamReader(inputStream);
-            BufferedReader buffer = new BufferedReader(reader)
+            BufferedReader buffer = new BufferedReader(reader);
+            PushbackReader pushbackReader = new PushbackReader(buffer);
         ) {
             int c;
-            while ((c = getc(buffer)) != -1) {
-                System.out.print((char)c);
+            while ((c = getc(pushbackReader)) != -1) {
+                result = (JsonPrimitive<?>) JsonPrimitiveParser.getParser(c).parse(pushbackReader);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * Получить следующий символ в файле (за исключением специальных символов)
-     * @param reader Объект класса BufferedReader, с помощью которого происходит посимвольное чтение
-     * @return Следующий символ в файле, не являеющийся специальным символом
-     */
-    private int getc(BufferedReader reader) {
-        try {
-            int c;
-            while ((c = reader.read()) != -1) {
-                if (!Character.isWhitespace(c)) {
-                    return c;
-                }
-            }
-        } catch (IOException e) {
+        } catch (IOException | JsonParserException e) {
             throw new RuntimeException(e);
         }
 
-        return -1;
+        return result;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         JsonParser parser = new JsonParser("test_json_file.json");
-        parser.parse();
+        System.out.println(parser.parse());
     }
 }
